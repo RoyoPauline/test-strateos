63a26a73bf501ade1df4cf3e02794d541f43c524
RoyoPauline <pauline.marine.royo@gmail.com>
2022-02-24 23:04:55 +0100

    Ajout du css et du filtre chronologique

--- test-strateos/src/App.jsx
+++ test-strateos/src/App.jsx
@@ -1,60 +1,102 @@
-import React from 'react';
-
-import { useState } from 'react';
-import UserCard from './components/UserCard';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import UserCard from "./components/UserCard";
 
 export default function App() {
+  const today = new Date();
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + " " + time;
+
   const [user, setUser] = useState({
-    firstname: '',
-    lastname: '',
-    phone: '',
+    firstname: "",
+    lastname: "",
+    phone: "",
+    createdAt: dateTime,
   });
   const [userList, setUserList] = useState([]);
 
+  const [ascendantFilter, setAscendantFilter] = useState(true);
+
   const submit = (user) => {
-    userList.push(user);
-    setUser({ firstname: '', lastname: '', phone: '' });
-    console.log(userList);
+    if (user.firstname.length !== 0) {
+      userList.push(user);
+      setUserList((list) =>
+        [...list].sort((a, b) => a.firstname.localeCompare(b.firstname))
+      );
+      setUser({ firstname: "", lastname: "", phone: "", createdAt: dateTime });
+    }
   };
   return (
-    <div>
+    <div className="container">
       <div>
-        <h1>Liste de vos contacts</h1>
+        <h1 className="title">Liste de vos contacts</h1>
         <p>Ajoutez un contact pour ne pas l'oublier</p>
-        <input
-          placeholder="Nom de famille"
-          type="text"
-          value={user.lastname}
-          onChange={(e) => setUser({ ...user, lastname: e.target.value })}
-        ></input>
-        <input
-          placeholder="Prénom"
-          type="text"
-          value={user.firstname}
-          onChange={(e) => setUser({ ...user, firstname: e.target.value })}
-        ></input>
-        <input
-          placeholder="Numéro de téléphone"
-          type="text"
-          value={user.phone}
-          onChange={(e) => setUser({ ...user, phone: e.target.value })}
-        ></input>
+        <form>
+          <div className="user-details">
+            <span ClassName="details"> Nom de famille</span>
+            <input
+              placeholder="Entrez votre nom de famille"
+              type="text"
+              value={user.lastname}
+              onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+            ></input>
+          </div>
+          <div className="user-details">
+            <span ClassName="details">Prénom</span>
+            <input
+              placeholder="Entrez votre prénom"
+              type="text"
+              value={user.firstname}
+              onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+            ></input>
+          </div>
+          <div className="user-details">
+            <span className="details">Numéro de téléphone</span>
+            <input
+              placeholder="Entrez votre numéro de téléphone"
+              type="text"
+              value={user.phone}
+              onChange={(e) => setUser({ ...user, phone: e.target.value })}
+            ></input>
+          </div>
+        </form>
 
-        <button onClick={() => submit(user)}>Ajouter</button>
-      </div>
-      <div>
-        {userList
-          .sort((a, b) => a.firstname.localeCompare(b.firstname))
-          .map((user) => {
-            return (
-              <UserCard
-                firstname={user.firstname}
-                lastname={user.lastname}
-                phone={user.phone}
-                order={user.order}
-              />
+        <button className="button" onClick={() => submit(user)}>
+          Ajouter
+        </button>
+
+        <button
+          className="button"
+          onClick={() => {
+            setUserList((list) =>
+              [...list].sort((a, b) => {
+                var dateA = new Date(a.createdAt),
+                  dateB = new Date(b.createdAt);
+                return ascendantFilter ? dateA - dateB : dateB - dateA;
+              })
             );
-          })}
+            setAscendantFilter(!ascendantFilter);
+          }}
+        >
+          Filtrer par ordre chronologique
+        </button>
+      </div>
+
+      <div>
+        {userList.map((user) => {
+          return (
+            <UserCard
+              firstname={user.firstname}
+              lastname={user.lastname}
+              phone={user.phone}
+              createdAt={user.createdAt}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- test-strateos/src/components/UserCard.jsx
+++ test-strateos/src/components/UserCard.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-const UserCard = ({ firstname, lastname, phone, order }) => {
+const UserCard = ({ firstname, lastname, phone, createdAt }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   return (
     <>
       {isVisible && (
-        <>
-          <button onClick={() => setIsVisible(false)}> Supprimer</button>
-          {firstname}
-          {lastname}
-          {phone}
-        </>
+        <div className="container">
+          <button className="button" onClick={() => setIsVisible(false)}>
+            {" "}
+            Supprimer
+          </button>
+          <div className="card">
+            <p className="subtitle">Prénom : {firstname} </p>
+            <p className="subtitle">Nom de famille : {lastname} </p>
+            <p className="subtitle">Numéro de téléphone : {phone} </p>
+            <p className="subtitle">Contact créé le : {createdAt} </p>
+          </div>
+        </div>
       )}
     </>
   );
